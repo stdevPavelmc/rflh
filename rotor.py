2aff901560efaf9dd61714a18423ec9955a72c2f
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2022-01-08 18:35:06 -0500

    Add rotor configuration from a file

--- rotor.py
+++ rotor.py
@@ -1,39 +1,80 @@
 import Hamlib
 import time
+import subprocess
+from configparser import ConfigParser
 
-ELMAX = 72
-AZMAX = 360
-MINSTEP = 5
+# default vars
+MINSTEP = 5.0
 
 class Rotor():
     def __init__(self):
-        self.rotorhost = "10.42.1.6"
-        self.rotorport = 4533
+        # load config and start the sub-process control if needed
+        self.loadconfig()
+        self.start_rotor()
 
+        # limits
+        self.MIN_AZ = 0
+        self.MIN_EL = 0
+        self.MAX_AZ = 360
+        self.MAX_EL = 90
+
+    def loadconfig(self):
+        # load config from the rotor.conf file
+        config = ConfigParser()
+
+        config.read('./rotor.conf')
+        self.default = config.get('DEFAULT', 'rotor')
+        self.model = config.get(self.default, 'model')
+        self.device = config.get(self.default, 'device')
+        self.options = config.get(self.default, 'options')
+
+    def start_rotor(self):
         # Disable all debug output from Hamlib
         Hamlib.rig_set_debug(Hamlib.RIG_DEBUG_NONE)
 
         # Create rotor object of type net
-        self.rot = Hamlib.Rot(Hamlib.ROT_MODEL_NETROTCTL)
+        # self.rot = Hamlib.Rot(Hamlib.ROT_MODEL_NETROTCTL)
+        self.rot = Hamlib.Rot(int(self.model))
 
-        # Setup network protocol
-        self.rot.set_conf("rot_pathname", self.rotorhost + ":" + str(self.rotorport))
+        # Setup device
+        self.rot.set_conf("rot_pathname", self.device)
+
+        # setup extra options 
+        opts = self.options.strip()
+        if len(opts) > 0:
+            if not (len(opt) % 2):
+                print("Strange, options must be in pairs, trying but may fail...")
+
+            pairs = []
+            cmds = []
+            s = opts.split(' ')
+            for i in range(len(s)/2):
+                cmd = s[i*2]
+                data = s[i*2 + 1]
+                if cmd == '-s':
+                    cmds.append(['serial-speed', data])
+                if cmd == '-C':
+                    cmds.append([data.split('=')[0],
+                                data.split('=')[1]])
+
+            for (opt, value) in cmds:
+                self.rot.set_conf(opt, value)
 
         # Open rotor
         # The Python bindings for Hamlib does not return anything
         # so we have no knowledge if this was actually successful...
         self.rot.open()
 
+        # get the limits from the rotor
+        self.MIN_AZ = float(self.rot.get_conf("min_az"))
+        self.MIN_EL = float(self.rot.get_conf("min_el"))
+        self.MAX_AZ = float(self.rot.get_conf("max_az"))
+        self.MAX_EL = float(self.rot.get_conf("max_el"))
+
     def set_position(self, az, elv):
-        # Value conditionign
-        if az > AZMAX:
-            az = AZMAX
-        if az < 0:
-            az = 0
-        if elv > ELMAX:
-            elv = ELMAX
-        if elv < 0:
-            elv = 0
+        # check limits
+        az = min(self.MAX_AZ, max(self.MIN_AZ, az))
+        elv = min(self.MAX_EL, max(self.MIN_EL, elv))
 
         # ask postion
         [aaz, ael] = self.get_position()
@@ -54,13 +95,19 @@ class Rotor():
     def go_to(self, az, elv=0):
         self.rot.set_position(az, elv)
 
+    def close(self):
+        # close all connections
+        if  self.rot:
+            self.rot.close()
+
 if __name__ == "__main__":
     r = Rotor()
     a = 0
-    for az in range(0, 95, 10):
+    for az in range(0, 90, 10):
         r.set_position(az, 0)
         (a, e) = r.get_position()
         print("Azimuth, actual: {}, set: {}".format(a, az))
 
     # park
     r.go_to(0, 0)
+    r.close()
