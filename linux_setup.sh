98d3a8e1bb2efd2768704183f924b9436e86a961
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2022-01-08 19:00:28 -0500

    Add the linux install script

---
+++ linux_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Setup script for any debian based distro (Debian, Ubuntu, Min, etc)
+
+# update package database
+sudo apt update
+
+# install python3 ans requisites
+sudo apt install -y python3 python3-pip
+
+# install rtl-sdr native support
+sudo apt install -y librtlsdr0 rtl-sdr librtlsdr-dev libusb-1.0-0
+
+# install python rtlsdr support
+pip3 install pyrtlsdr
+
+# install hackrf native support
+sudo apt install -y libhackrf0 libhackrf-dev hackrf
+
+# install support for hamlib (rotctld) python included
+sudo apt install -y libhamlib2 libhamlib-utils python3-libhamlib2
+
+# install app dependencies
+pip3 install matplotlib numpy scipy
+
+# end
+echo "Done!"
